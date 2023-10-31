8a91cd01a1149d8a0f39ac2a3fd59755986861d7
ShivanshSinghnnovative <shivansh.singh@innovative.in>
2023-10-31 17:53:32 +0530

    firebase

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class=" bg-red-500 text-xl text-red-500 ">
+    <p class="text-xl border-2 ">
+    ojk
+     </p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
